47fc50ac196c2c989434cb209b2a966a93152993
Galen Reed <galen.d.reed@gmail.com>
2019-01-18 10:56:47 -0800

    updates

--- example_GFR.m
+++ example_GFR.m
@@ -1,5 +1,6 @@
 clear all;
 close all;
+pkg load image;
 options = optimset('Display','off');
 
 % 0 for regularized inverse, 1 for thresholded SVD
@@ -47,16 +48,34 @@ end
 
 % make a montage of the zoomed in kidney
 % montage doesnt work though
-xlimits = 100:200; ylimits = 40:110;
-plotMatrix = zeros([length(xlimits), length(ylimits), 1, Nt]);
-for ii = 1:Nt
-  thisImage = squeeze(im(xlimits, ylimits, 1, ii));
-  plotMatrix(:,:,1,ii) = thisImage;  
-end
+##xlimits = 100:200; ylimits = 40:110;
+##for ii = 1:Nt
+##  thisImage = squeeze(im(xlimits, ylimits, 1, ii));
+##  figure();
+##  imagesc(thisImage);
+##  set(gca, 'xtick', [], 'ytick', []);
+##  colormap gray;  
+##end
 
+
+uptakeInds = 2:5; % time indices of uptake
+medullaVolume = 0.2; % not actually measured
+P = polyfit (t(uptakeInds), medulla(uptakeInds), 1);
+fit = polyval(P, t(uptakeInds));
+gfr = P(1) * medullaVolume / mean(cortex(uptakeInds));
+
+
+fontsize = 20;
+ lw = 2;
 figure()
-plot(t, cortex, '.-',...
-     t, medulla, '.-')
-legend('cortex ROI', 'medulla ROI')
+plot(t, cortex, 'o-', 'linewidth', lw,...
+     t, medulla, 'o-','linewidth', lw,...
+     t(uptakeInds), fit, '--','linewidth', 2*lw);
+xlim([0 t(end)]);
+legend('cortex ROI', 'medulla ROI', 'fit during uptake')
 xlabel('time [s]');
 ylabel('signal');
+    set(gca, 'fontsize', fontsize);
+
+    
+    
